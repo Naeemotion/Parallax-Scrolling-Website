d131db3d207392946a4b6291b64090d5114efcbe
Mohammad Naeem Hossain <naeemhossain0818@gmail.com>
2023-03-19 08:15:28 +0600

    all done

--- script.js
+++ script.js
@@ -7,4 +7,8 @@ let hill5 = document.getElementById('hill5');
 window.addEventListener('scroll',() => {
     let value = window.scrollY;
     text.style.marginTop = value * 2.5 +'px';
+    leaf.style.top = value * -1.5 +'px';
+    hill5.style.left = value * 1.5 +'px';
+    hill4.style.left = value * -1.5 +'px';
+    hill1.style.top = value * 1 +'px';
 })
